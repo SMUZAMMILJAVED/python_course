aec9d2d0dce5e49163e762ae18055d2631f8f943
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2025-12-19 14:50:49 +0500

    chapter 1 to 9

---
+++ class_01/main.py
@@ -0,0 +1,37 @@
+                    
+                      # book => smarter way to learn python
+
+# data types and print function 
+# print('any thing in quotes')
+# print(4534)
+# print(False)
+# print(True)
+
+                             
+                                     # variables 
+# na53me=76
+# age=22
+# isStudent=True
+# print(na53me,age,isStudent)
+
+                                #   math operators 
+
+# a=4
+# print(3+3+a)
+# print(10%2)
+# age =22
+# age*=2
+# print(age)
+
+                           # concatination strings 
+  
+# name='muzammil'
+# age='2'
+# print(name + age)
+
+                               # if statement 
+# num=0
+# if num>12:
+#     print('numbr is greater than 12')
+
+# print('outside from if')
