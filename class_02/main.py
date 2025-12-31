6dceca9e568fc0c22786d36ec6df4446c7ee6fa8
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2025-12-31 15:43:50 +0500

    Create main.py

---
+++ class_02/main.py
@@ -0,0 +1,23 @@
+# print('hello world!')
+                        # type casting unfemilar operators 
+# result='45'
+# result = float(result)
+# result=5
+# result%=2
+# result+=2
+# result-=2
+# result/=2
+# result*=2
+# print(result+2)
+
+                                 # concatinating   strings 
+# print('hello ' + " 1 "+ 'world')
+                                 #   if else elif 
+
+# num=4
+# if num!=4:
+#     print('hello world')
+# elif num>2:
+#     print('test')
+                         # comparision operators 
+print('c'=='c')
