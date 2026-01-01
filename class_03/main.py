0d4a4620a08e7bbe4a3cbe21f5bcd3e27cd8051f
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2026-01-02 01:08:36 +0500

    chapter 11 to 15

---
+++ class_03/main.py
@@ -0,0 +1,44 @@
+                           #  if/else 
+ # num=6
+# if num==5:
+#     num=10
+# else:
+#     num=15
+                           # type casting 
+# print(num)
+# num=input('write an number')
+# print(int(num)+2)
+                          # nested if else 
+# percentage=164634
+# if percentage>=0 and percentage<=100:
+#     if percentage>=80:
+#         print('A+')
+#     elif percentage>=70:
+#         print('A')
+#     elif percentage>=60:
+#         print('B')
+#     elif percentage>=50:
+#         print('C')
+#     elif percentage>=40:
+#         print('D')
+#     else:
+#         print('fail')
+# else:
+#     print('invalid number!')
+
+
+
+# testing set of condition
+# print("" or False)
+
+
+                  # testing set of condition / elif / else 
+# num=20
+# if num%3==0 and num%5==0:
+#     print('both three and five')
+# elif num%3==0:
+#     print('only three')
+# elif num%5==0:
+#     print('only five')
+# else:
+#     print('not in three and five')
