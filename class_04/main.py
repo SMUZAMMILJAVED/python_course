216b2e8bafc793abc681fca06b3816a300a3c1c7
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2026-01-05 15:15:24 +0500

    chapter 15 to 17

---
+++ class_04/main.py
@@ -0,0 +1,16 @@
+#                                # list 
+
+# list=['apple',23,False,2.45]   #diffrent types of data
+# list[3]='new data'   #update data
+# list.append('append')   #add element in last
+# list.insert(1,'2nd element')   #add slement specific index
+# newItems=['item1',4,'item4']
+# list.extend(newItems)   #add complte array items
+# print(list);         
+
+#                                      # slice 
+
+list=['apple',23,False,2.45]
+print(list[2:3])                    #   index,index but not include last 
+print(list[2:])                    #   end tk jaega 
+print(list[-3:])                    #   Nagative slicing
