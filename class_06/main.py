b12b6e410f92133a7269032df268832a444c21f6
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2026-01-08 21:05:49 +0500

    chapter 21 to 24

---
+++ class_06/main.py
@@ -0,0 +1,64 @@
+
+#                                     # table of 2 
+
+# # for i in range(1,11):
+# #     print("2 x ",i," = ",2*i)
+
+
+#                              # find user city name 
+
+# # cities=['karachi','lahore','islamabad']
+# # userCity ='LahoRE'.lower()
+# # found=False
+# # for city in cities:
+# #     if city==userCity:
+# #         print(city,": found!")
+# #         found=True
+# #         break  
+# # if(found !=True):
+# #     print(userCity,": not found!")
+
+
+#                        # nested for loop
+# for i in range(0,3):
+#     for j in range(0,3):
+#         for k in range(0,3):
+#             print("hello wordl!")  #27 times
+
+
+                       
+                       #nested for loop iterate
+
+# items=[[1,2,3],['a','b','c']]
+# for item in items:
+#     for i in item:
+#         print(i)
+
+          
+                       #nested for loop searching
+
+# items=[['karachi','lahore'],['islmabad','quetta']]
+# city='KARACHI'.lower()
+# found=False
+# for item in items:
+#     for i in item:
+#         if i==city:
+#             print(city,": found")
+#             found=True
+#             break
+
+# if(found !=True):
+#     print(city,': not found')
+
+                        # input and type convertion 
+# name = input('what is your name?')
+# print("Hello !",name.title())
+
+
+# num = input('what is fav num?')
+# num = float(num)
+# print("your num  plus 5  = ",num +5)
+
+# num = input('what is fav num?')
+# num = str(num)
+# print("your num  in convert in str = ",num +" test")
