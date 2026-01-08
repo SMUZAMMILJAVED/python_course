a256927cd77442513ea81ef12f6cdd084e28a83d
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2026-01-08 19:51:38 +0500

    chapter 18 to 21

---
+++ class_05/main.py
@@ -0,0 +1,26 @@
+#                      # deleting and removing elements
+
+# list =['item1',4,2,True,45.53]
+# del list[1]   #del specific element 
+# list.remove('item1') # reomve by value
+# list.pop(1) #remove by index no
+# list.pop()  #remove last element
+# print(list)
+
+                             # tupples immutable
+# list =('item1',4,2,True,45.53)
+# list[2]='test'   # Error because tupples not allow to update value
+# list[0:2]  #slice work
+# print(list[0]) 
+
+                                  # for loop
+                                  
+# for i in range(0,11):
+#     print(i)
+
+                          # for lopp for list and tupples 
+
+# list =('item1',4,2,True,45.53)
+# list2 =['item1',4,2,True,45.53]
+# for item in list2:
+#     print(item)
